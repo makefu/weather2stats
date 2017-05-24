2090751f1f0af81da2fbdfbfa22eeb1efb35d322
makefu <github@syntax-fehler.de>
2017-05-24 23:27:47 +0200

    opensensemap: rewrite

--- opensensemap.py
+++ opensensemap.py
@@ -4,32 +4,48 @@ import requests
 import sys,json
 import graphite
 
-api_end= "http://opensensemap.org:8000/"
-host="heidi.shack"
+api_end= "https://api.opensensemap.org/"
 box= "56a0de932cb6e1e41040a68b" #shackspace
-mapping = { "UV-Intensität": "global_radiation",
+mapping = { "UV-Intensität": "uv_intensity",
             "Temperatur": "temperature",
             "rel. Luftfeuchte": "humidity",
             "Luftdruck": "pressure",
             "Beleuchtungsstärke": "brightness" }
 
 to = dt.datetime.now().isoformat()
-sense_params = { 
-                 # "from-date": "2016-05-12T21:11:12.621978",
-                 # "to-date": "2016-06-12T21:11:12.621978", 
-                 "to-date": to, 
-                 "from-date": "", 
-                 "format":"JSON" }
+sense_params = {
+    # "from-date": "2016-05-12T21:11:12.621978",
+    # "to-date": "2016-06-12T21:11:12.621978", 
+    "to-date": to,
+    "from-date": "",
+    "format":"JSON"
+}
 
 data = []
-base_key = "weather.sensebox.shackspace."
-for sense in requests.get(api_end+"/boxes/"+box).json()['sensors']:
-    name = sense['title']
-    if name in mapping:
-        ret = requests.get("{}/boxes/{}/data/{}".format(api_end,box,sense['_id']),
-                params=sense_params).json()
-        for kv in ret:
-            ts = int(dt.datetime.strptime(kv['createdAt']+"+0000","%Y-%m-%dT%H:%M:%S.%fZ%z").timestamp())
-            data.append([base_key+mapping[name],float(kv['value']),ts])
 
-graphite.send_all_data(data,host=host)
+
+def get_sensemap(boxid):
+    info = requests.get(api_end+"/boxes/"+boxid).json()
+    val = {
+        "_id": boxid,
+        "name": info['name'],
+        # TODO: createdAt may be different for different sensors, we take the
+        # latest val
+        "ts": int(dt.datetime.strptime(info['updatedAt']+"+0000","%Y-%m-%dT%H:%M:%S.%fZ%z").timestamp())
+    }
+    # TODO: original code used /boxes/boxid/data/sensorid to fetch all previous points
+    # ts = int(dt.datetime.strptime(kv['createdAt']+"+0000","%Y-%m-%dT%H:%M:%S.%fZ%z").timestamp())
+    for sense in info['sensors']:
+        name = sense['title']
+        kv = sense['lastMeasurement']
+        if name in mapping:
+            val[mapping[name]] = float(kv['value'])
+    return val
+
+def main(boxid):
+    ret = get_sensemap(boxid)
+    print(json.dumps(ret))
+
+if __name__ == "__main__":
+    main(box)
+    # graphite.send_all_data(data,host=host)
