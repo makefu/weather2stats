0e191c75c1c13d1ba0bd0f7804d7999c41c77161
makefu <github@syntax-fehler.de>
2017-05-24 22:28:18 +0200

    refactor openweathermap

--- openweathermap.py
+++ openweathermap.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python
 import sys
-import requests,json
+import json
+import requests
 import graphite
-openweathermap_apikey = ""
-host = "heidi.retiolum"
+openweathermap_apikey = "***REMOVED***"
+
 def k2c(t):
     """ kelvin to degree celsius """
-    return t-273.15
+    return float(t)-273.15
 
-kvt = []
-d = requests.get('http://api.openweathermap.org/data/2.5/group?id=2825297,6930414,2867993,2927043&appid={}'.format(openweathermap_apikey)).json()
-for l in d['list']:
-    sensor = "weather.openweathermap." + l['name']
-    # middle of the hour
-    ts = l['dt'] - 1800
 
-    for k,v in l['main'].items():
-        if "temp" in k:
-            k = k.replace("temp","temperature")
-            v = k2c(v)
-        kvt.append([ sensor+"."+k,v,ts])
+def get_openweathermap():
+    kvt = []
+    d = requests.get('http://api.openweathermap.org/data/2.5/group?id=2825297,6930414,2867993,2927043&appid={}'.format(openweathermap_apikey)).json()
+    for l in d['list']:
+        val = {
+            "name": l['name'],
+            "ts": l['dt'] - 1800
+        }
 
-graphite.send_all_data(kvt,host=host)
+        for k,v in l['main'].items():
+            if "temp" in k:
+                k = k.replace("temp","temperature")
+                v = k2c(v)
+            val[k] = v
+        # 'wind': {'speed': 2.6, 'deg': 340}
+        for k,v in l['wind'].items():
+            val["wind_"+k] = v
+        kvt.append(val)
+    return kvt
+
+def main():
+    kvt = get_openweathermap()
+    print(json.dumps(kvt))
+    # graphite.send_all_data(kvt,host="heidi.shack")
+
+if __name__ == "__main__":
+    main()
