cc806de01c47891371c15eb0d4fbb428242e7fc3
makefu <github@syntax-fehler.de>
2017-05-26 22:47:03 +0200

    rm runall.sh

--- runall.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-(while sleep 15m; do python3 stadtklima-stuttgart.py ; echo "`date`: done"; done)&
-(while sleep 30m; do python3 openweathermap.py; echo "`date`:done" ;done)&
-(while sleep 30m; do python3 opensensemap.py ;echo "`date`:done" ; done)&
-echo "waiting for all"
-wait
