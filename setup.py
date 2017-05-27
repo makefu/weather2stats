91c12f232b108122bdd4a21153425061f345b5f2
makefu <github@syntax-fehler.de>
2017-05-28 00:25:38 +0200

    init setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+setup(
+    name='weather2stats',
+    packages=find_packages(),
+    version='0.1',
+    description='Convert stats websites to influx data',
+    author='Felix Richter',
+    license='WTFPL',
+    author_email='github@syntax-fehler.de',
+    url='https://github.com/makefu/weather2stats',
+    install_requires = [
+        'requests',
+        'pytz',
+        'beautifulsoup4',
+        'docopt',
+        'influxdb'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'to-influx=weather2stats.to_influx:main'
+        ],
+    },
+
+    keywords=['api'],
+)
